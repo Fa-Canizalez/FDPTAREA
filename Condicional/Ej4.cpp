cdf069a9b679cec8553ed1b4f05dec6cdbe422e3
Fa-Canizalez <00023726@uca.edu.sv>
2026-04-29 08:23:25 -0600

    :memo: Add: Ejercicio4

---
+++ Condicional/Ej4.cpp
@@ -0,0 +1,29 @@
+/*Ejercicio #4 nivel medio IF, ELSE, ELSE IF
+Calificación Aprobatoria
+Solicita una calificación numérica y muestra si el estudiante aprobó o reprobó (mínimo aprobatorio: 60).*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int nota;
+
+    cout << endl << "QUIRES SABER SI APROBASTE O NO?" << endl << endl;
+    cout << "Ingresa tu nota: "<< endl;
+    cin >> nota;
+
+    if (nota >=75)
+    {
+        cout << "FELICIDADES! HAZ APROBADO CON " << nota << " :D" << endl;
+    }
+    else if (nota < 75 && nota >= 60)
+    {
+        cout << "Haz aprobado con " << nota << ", mejoremos esa nota un poco mas ;)" << endl;
+    }
+    else 
+    {
+        cout << "Haz reprobado :c" << endl << "Esforzemonos mas la proxima!" << endl;
+    }
+    return 0;
+}
